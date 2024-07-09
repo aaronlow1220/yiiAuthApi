a87544c1d8f5c4a71fd94cff65fc3d5a9ccd1442
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-07-09 14:11:47 +0800

    remove unnecessary code

--- config/console.php
+++ config/console.php
@@ -5,11 +5,9 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'app\commands',
     'components' => [
         'db' => $db,
     ],
-    'params' => $params,
 ];
 
 return $config;
