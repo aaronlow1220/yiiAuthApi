8504eb52f30ceebdd0c138ea960d5354898f13c6
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-07-10 17:09:13 +0800

    remove unnecessary code

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -19,22 +19,4 @@ class LoginForm extends Model
             ['email', 'email']
         ];
     }
-
-    public function login()
-    {
-        if (!$this->validate()) {
-            return false;
-        }
-
-        $this->_user = User::getUser($this->email);
-
-        if (!$this->_user || !$this->_user->validatePassword($this->password)) {
-            return false;
-        }
-
-        $access_token = $this->_user->generateAccessToken();
-        $this->_user->access_token = $access_token;
-        $this->_user->update();
-        return $access_token;
-    }
 }
