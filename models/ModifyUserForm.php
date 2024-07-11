737446ad77e05b6c82894888c579910e0e614eb8
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-07-11 17:56:07 +0800

    Add new annotation to ModifyUserForm

--- models/ModifyUserForm.php
+++ models/ModifyUserForm.php
@@ -7,10 +7,25 @@ use yii\base\Model;
 
 class ModifyUserForm extends Model
 {
+    /**
+     * @var string $username Username of the user.
+     */
     public $username;
+
+    /**
+     * @var string $email Email of the user.
+     */
     public $email;
+
+    /**
+     * @var string $password Password of the user.
+     */
     public $password;
 
+    /**
+     * Validation rules for the form.
+     * @return string[][] Return validation rules.
+     */
     public function rules()
     {
         return [
