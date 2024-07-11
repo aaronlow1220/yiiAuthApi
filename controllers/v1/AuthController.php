e88be67bc0c8b9dc1116569a7717f4ae0cee8886
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-07-11 16:08:24 +0800

    OA comments added to AuthController

--- controllers/v1/AuthController.php
+++ controllers/v1/AuthController.php
@@ -10,10 +10,14 @@ use yii\web\HttpException;
 use yii\web\Response;
 
 /**
+ * @OA\Tag(
+ *      name="Auth",
+ *      description="Authentication API"
+ * )
+ * 
  * Controller for handling authentication.
  * 
- * @author aaronlow <aaron.low@atelli.ai>
- * version: 1.0
+ * version: 1.0.0
  */
 class AuthController extends Controller
 {
@@ -30,6 +34,34 @@ class AuthController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *      path="/v1/auth/register",
+     *      summary="Register a user",
+     *      description="Register a user",
+     *      tags={"Auth"},
+     *      @OA\RequestBody(
+     *          description="User credentials",
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(property="username", type="string", description="username"),
+     *                  @OA\Property(property="email", type="string", description="email"),
+     *                  @OA\Property(property="password", type="string", description="password"),
+     *                  @OA\Property(property="conformPassword", type="string", description="confirm password")
+     *              )
+     *          )
+     *      )
+     *      @OA\Response(
+     *          response=200,
+     *          description="User registered successfully",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *                  @OA\Property(property="uuid", type="string", description="uuid"),
+     *          )
+     *      )
+     * )
+     * 
      * Register a user.
      * 
      * @return array | Response Return the user data. If the data is invalid, return error messages.
@@ -49,6 +81,32 @@ class AuthController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *      path="/v1/auth/login",
+     *      summary="login a user",
+     *      description="login a user",
+     *      tags={"Auth"},
+     *      @OA\RequestBody(
+     *          description="User credentials",
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(property="email", type="string", description="email"),
+     *                  @OA\Property(property="password", type="string", description="password")
+     *              )
+     *          )
+     *      )
+     *      @OA\Response(
+     *          response=200,
+     *          description="User logged in successfully",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *                  @OA\Property(property="access_token", type="string", description="access token"),
+     *          )
+     *      )
+     * )
+     * 
      * Login a user.
      * 
      * @return array | Response Return the access token. If the data is invalid, return error messages.
@@ -67,6 +125,21 @@ class AuthController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *      path="/v1/auth/logout",
+     *      summary="logout a user",
+     *      description="logout a user",
+     *      tags={"Auth"},
+     *      @OA\Response(
+     *          response=200,
+     *          description="User logged out successfully",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *                  @OA\Property(property="message", type="string", description="logout message"),
+     *          )
+     *      )
+     * )
+     * 
      * Logout a user.
      * 
      * @throws HttpException If the user is not found.
