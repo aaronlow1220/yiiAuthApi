4f6f3c6deee7896b1efc0a8b9b5cb7ed5fe46cbe
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-07-15 11:03:59 +0800

    migration updated

--- migrations/m240702_054625_create_users_table.php
+++ migrations/m240702_054625_create_users_table.php
@@ -14,16 +14,18 @@ class m240702_054625_create_users_table extends Migration
     public function safeUp()
     {
         $this->createTable('users', [
-            "id" => $this->primaryKey(),
-            "uuid" =>  $this->string(255)->notNull(),
+            "id" => $this->bigPrimaryKey()->unsigned()->notNull()->comment('編號'),
+            "uuid" => $this->string(255)->notNull(),
             "username" => $this->string(255),
-            "email"=> $this->string(255)->notNull(),
-            "password"=> $this->string(255)->notNull(),
-            "status"=> $this->smallInteger(1)->notNull()->defaultValue(0),
-            "auth_key"=> $this->string(255),
-            "access_token"=> $this->string(255),
-            "created_at"=> $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
-            "updated_at"=> $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->append('ON UPDATE CURRENT_TIMESTAMP'),
+            "email" => $this->string(255)->notNull(),
+            "password" => $this->string(255)->notNull(),
+            "status" => $this->smallInteger(1)->notNull()->defaultValue(0),
+            "auth_key" => $this->string(255),
+            "access_token" => $this->string(255),
+            "created_at" => $this->integer(10)->unsigned()->notNull()->comment('unixtime'),
+            "updated_at" => $this->integer(10)->unsigned()->notNull()->comment('unixtime'),
+            "created_by" => $this->bigInteger(20)->unsigned()->notNull()->comment('ref:users.id'),
+            "updated_by" => $this->bigInteger(20)->unsigned()->notNull()->comment('ref:users.id'),
         ]);
     }
 
