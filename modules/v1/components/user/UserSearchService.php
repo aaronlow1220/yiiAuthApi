2ab6695f8be7d7a92342c5e9bf1184ba19823413
aaronlow1220 <117196390+aaronlow1220@users.noreply.github.com>
2024-07-16 16:02:44 +0800

    feat: UserSearchService to search user

---
+++ modules/v1/components/user/UserSearchService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace v1\components\user;
+
+use app\components\user\UserRepository;
+use yii\data\ActiveDataProvider;
+
+class UserSearchService{
+    protected  $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+    public function searchUser(array $criteria): ActiveDataProvider
+    {
+        $query = $this->userRepository->search($criteria);
+        return new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC,
+                ],
+            ],
+        ]);
+    }
+}
